d3a1e0b082f89d542c5da16f7d09c51604f5147e
Gopi <31792031+gopikiller@users.noreply.github.com>
2021-12-19 08:08:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect("localhost", "saveitsa_gopi", "gopikiller7537", "saveitsa_bot");
+$db = mysqli_connect("localhost", "saveitsa_gopi", "PASSWORD", "saveitsa_bot");
 if (!$db) {
     echo 'not connected';
     die();
